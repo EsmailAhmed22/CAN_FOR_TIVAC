b5ae84da889bd639b4cd6a3c060da1bdc78e4651
Esmail Ahmed Abdelaziz <esmailahmed27211@gmail.com>
2021-09-07 06:12:42 +0200

    Mapping All CAN Registers

--- CAN_Driver/can_registers.h
+++ CAN_Driver/can_registers.h
@@ -36,7 +36,49 @@
 #define PORT_ANALOG_MODE_SEL_REG_OFFSET   0x528
 #define PORT_CTL_REG_OFFSET               0x52C
 
-/* CAN Registers for CAN configuration */
+/* CAN Base Address */
+#define CAN_0_BASE_ADDRESS       (*((volatile uint32 *)0x40040000))
+#define CAN_1_BASE_ADDRESS       (*((volatile uint32 *)0x40041000))
+
+/* CAN Registers offset addresses */
+#define CAN_CONTROL_REG					 0x000	
+#define CAN_STATUS_REG					 0x004	
+#define CAN_ERROR_COUNTER_REG		 0x008	
+#define CAN_BIT_TIMING_REG			 0x00C	
+#define CAN_INTERRUPT_REG				 0x010	
+#define CAN_TEST_REG					   0x014	
+#define CAN_PRESCALER_EXT_REG	   0x018	
+#define CAN_IF1_COMM_REQ_REG		 0x020	
+#define CAN_IF2_COMM_REQ_REG		 0x080	
+#define CAN_IF1_COMM_MASK_REG		 0x024	
+#define CAN_IF2_COMM_MASK_REG		 0x084	
+#define CAN_IF1_MASK1_REG				 0x028	
+#define CAN_IF2_MASK1_REG				 0x088	
+#define CAN_IF1_MASK2_REG				 0x02C	
+#define CAN_IF2_MASK2_REG				 0x08C		
+#define CAN_IF1_ARB1_REG				 0x030	
+#define CAN_IF2_ARB1_REG				 0x090		
+#define CAN_IF1_ARB2_REG				 0x034	
+#define CAN_IF2_ARB2_REG				 0x094
+#define CAN_IF1_MSG_CTL_REG			 0x038	
+#define CAN_IF2_MSG_CTL_REG			 0x098
+#define CAN_IF1_DATA_A1					 0x03C
+#define CAN_IF2_DATA_A1					 0x09C
+#define CAN_IF1_DATA_A2					 0x040
+#define CAN_IF2_DATA_A2					 0x0A0
+#define CAN_IF1_DATA_B1					 0x044
+#define CAN_IF2_DATA_B1					 0x0A4
+#define CAN_IF1_DATA_B2					 0x048
+#define CAN_IF2_DATA_B2					 0x0A8
+#define CAN_TRANS_REQ1					 0x100
+#define CAN_TRANS_REQ2					 0x104
+#define CAN_NEW_DATA1						 0x120
+#define CAN_NEW_DATA2						 0x124
+#define CAN_INT_PEND1						 0x140		
+#define CAN_INT_PEND2						 0x144	
+#define CAN_MSG_VALID1					 0x160
+#define CAN_MSG_VALID2					 0x164
+
 
 
 /* NVIC Registers for CAN Interrupt handling */
