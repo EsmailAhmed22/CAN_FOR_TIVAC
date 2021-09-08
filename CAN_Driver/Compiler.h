1a1445c2cfd091061e85b7da14c2380bffbeebec
Esmail Ahmed Abdelaziz <esmailahmed27211@gmail.com>
2021-09-08 07:03:24 +0200

    Abstract Compiler dependent instructions

---
+++ CAN_Driver/Compiler.h
@@ -0,0 +1,21 @@
+ /******************************************************************************
+ *
+ * Compiler symbol: Arm Compiler Version 6
+ *
+ * File Name: Compiler.h
+ *
+ * Description:  This file contains the definitions and macros for
+ *							 the abstraction of compiler specific keywords.
+ *
+ * Author: Esmail Ahmed
+ *
+ *******************************************************************************/
+#ifndef COMPILER_H
+#define COMPILER_H
+
+
+
+/* The compiler abstraction shall provide the NULL_PTR define with a void pointer to zero definition. */
+#define NULL_PTR        ((void *)0)
+
+#endif  /*COMPILER_H*/
